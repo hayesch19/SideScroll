5e9af86506e24edfb0379cdfc46056e7c17bd5e4
Chris Hayes <chrishayespro@gmail.com>
2025-06-10 19:57:59 -0400

    Project Setup & Charater Creation

---
+++ index.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 40
+        this.height = 40
+    }
+    draw() {
+        c.fillStyle = 'orange'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+player.draw()
