587149d0989c7973dda413f5533d470257122356
Chris Hayes <chrishayespro@gmail.com>
2025-06-13 17:21:34 -0400

    Platform & Collision Detection Added

--- index.js
+++ index.js
@@ -35,7 +35,24 @@ class Player {
     }
 }
 
+class Platform {
+    constructor (){
+        this.position = {
+            x: 500,
+            y: 500
+        }
+        this.width = 200
+        this.height = 40
+    }
+
+    draw() {
+        c.fillStyle = 'green'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 const player = new Player()
+const platform = new Platform()
 const keys = {
     right: {
         pressed: false
@@ -49,12 +66,21 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
     if (keys.right.pressed) {
         player.velocity.x = 5
     } else if (keys.left.pressed) {
         player.velocity.x = -5
     }else player.velocity.x = 0
+
+    //Platform Collision Detection
+    if (player.position.y + player.height <= platform.position.y && 
+        player.position.y + player.height + player.velocity.y >= platform.position.y && 
+        player.position.x + player.width >= platform.position.x && 
+        player.position.x <= platform.position.x + platform.width ) {
+        player.velocity.y = 0
+    }
 }
 
 animate()
